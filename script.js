483344d62fce16fc6b368a8445d01554e951dcaf
milaiablks <camila.iablonski.santos@escola.pr.gov.br>
2025-07-01 19:58:53 +0000

    commit 3

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
 
-function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
-} 
+    campoResultado.textContent = palavras.join(", ");
+}
